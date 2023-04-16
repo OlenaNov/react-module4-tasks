2fe79b2d8a889362c0deea863879f7296acfcdb2
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-04-16 12:28:05 +0200

    start

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Layout = styled.main`
+    display: flex;
+    flex-direction: column;
+    gap: 28px;
+
+    width: 800px;
+    min-height: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 40px;
+    background-color: #DCC7AA
+`;
+
+export default Layout;
